ed1b275a950af7eb144ada188336573b9f21b2ed
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 22:45:56 -0400

    added component structure

---
+++ src/components/Info.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Sponsors from './Sponsors.component';
+
+const Info = () => {
+  return (
+    <div>
+      <h1>Make remote work</h1>
+      <p>
+        Get your team in sync, no matter your locationi. Streamline
+        processes, create team rituals, and watch productivity soar
+      </p>
+      <button>Learn more</button>
+      <Sponsors />
+    </div>
+  );
+};
+
+export default Info;
