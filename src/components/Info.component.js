4362bd7429121b8b2283909aa6454b6168215792
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 23:07:32 -0400

    styled for mobile

--- src/components/Info.component.js
+++ src/components/Info.component.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-import Clients from './Clients.component';
-
 const Info = () => {
   return (
-    <div>
-      <h1>Make remote work</h1>
-      <p>
+    <div className="text-center space-y-6">
+      <h1 className="text-4xl font-bold mt-10">Make remote work</h1>
+      <p className="text-medium-gray">
         Get your team in sync, no matter your locationi. Streamline
         processes, create team rituals, and watch productivity soar
       </p>
-      <button>Learn more</button>
-      <Clients />
+      <button className="bg-almost-black text-almost-white py-2 px-4 rounded-xl text-sm">
+        Learn more
+      </button>
     </div>
   );
 };
