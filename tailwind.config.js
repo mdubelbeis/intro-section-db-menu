c67ecb5cfd96c708fdb7899b0c9623b57f9fda57
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 23:08:00 -0400

    modified custom color style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       colors: {
         'almost-white': 'hsl(0,0%,98%)',
-        'medium-gray': 'hsl(0,0%,98%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
         'almost-black': 'hsl(0,0%,8%)',
       },
       fontFamily: {
