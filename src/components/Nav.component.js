b2263ff46f4ba76a7eba7024f0e86c4759053aba
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-26 01:04:04 -0400

    added user functionality

--- src/components/Nav.component.js
+++ src/components/Nav.component.js
@@ -27,12 +27,39 @@ const navItems = [
   },
 ];
 
-const Nav = () => {
-  const [arrow, setArrow] = useState(`${downArrow}`);
+const arrow = {
+  open: `${upArrow}`,
+  closed: `${downArrow}`,
+};
+
+const Nav = ({ showMobileNav }) => {
+  const [featureArrow, setFeatureArrow] = useState(arrow.closed);
+  const [companyArrow, setCompanyArrow] = useState(arrow.closed);
+  const [openFeatures, setOpenFeatures] = useState(false);
+  const [openCompany, setOpenCompany] = useState(false);
+
+  const onFeatureClick = () => {
+    featureArrow === arrow.closed
+      ? setFeatureArrow(arrow.open)
+      : setFeatureArrow(arrow.closed);
+
+    setOpenFeatures(!openFeatures);
+  };
+
+  const onCompanyClick = () => {
+    companyArrow === arrow.closed
+      ? setCompanyArrow(arrow.open)
+      : setCompanyArrow(arrow.closed);
+
+    setOpenCompany(!openCompany);
+  };
 
   return (
     <div className="text-medium-gray p-4 text-sm">
-      <div className="fixed top-4 right-4 hover:cursor-pointer">
+      <div
+        className="fixed top-4 right-4 hover:cursor-pointer"
+        onClick={() => showMobileNav(false)}
+      >
         <img
           className="bg-medium-grey"
           src={menuClose}
@@ -42,32 +69,51 @@ const Nav = () => {
 
       {/*///// FEATURES */}
       <div className="flex flex-col gap-4 mt-16">
-        <div className="flex items-center gap-4 hover:cursor-pointer">
+        <div
+          className="flex items-center gap-4 hover:cursor-pointer"
+          onClick={onFeatureClick}
+        >
           <h3 className="text-base">Features</h3>
-          <img className="w-2 h-2" src={arrow} alt="arrow menu" />
+          <img
+            className="w-2 h-2"
+            src={featureArrow}
+            alt="arrow menu"
+          />
         </div>
-        {navItems.map((item) => {
-          return (
-            <div className="flex gap-2 items-center px-6 hover:cursor-pointer hidden">
-              <img className="w-4 h-4" src={item.icon} alt="" />
-              <p>{item.text}</p>
-            </div>
-          );
-        })}
+        {openFeatures &&
+          navItems.map((item) => {
+            return (
+              <div className="flex gap-2 items-center px-6 hover:cursor-pointer">
+                <img className="w-4 h-4" src={item.icon} alt="" />
+                <p>{item.text}</p>
+              </div>
+            );
+          })}
       </div>
 
-      {/* //// ABOUT */}
+      {/* //// COMPANY */}
       <div className="flex flex-col gap-4 mt-4">
-        <div className="flex items-center gap-[11px] hover:cursor-pointer">
+        <div
+          className="flex items-center gap-[11px] hover:cursor-pointer"
+          onClick={onCompanyClick}
+        >
           <h3 className="text-base">Company</h3>
-          <img className="w-2 h-2" src={arrow} alt="arrow menu" />
+          <img
+            className="w-2 h-2"
+            src={companyArrow}
+            alt="arrow menu"
+          />
         </div>
-        <div className="space-y-4 px-6 hidden">
-          <p className="hover:cursor-pointer">History</p>
-          <p className="hover:cursor-pointer">Our Team</p>
-          <p className="hover:cursor-pointer">Blog</p>
-        </div>
+        {openCompany && (
+          <div className="space-y-4 px-6">
+            <p className="hover:cursor-pointer">History</p>
+            <p className="hover:cursor-pointer">Our Team</p>
+            <p className="hover:cursor-pointer">Blog</p>
+          </div>
+        )}
       </div>
+
+      {/* //// OTHER */}
       <div className="mt-4 hover:cursor-pointer">
         <h3 className="text-base">Careers</h3>
       </div>
@@ -80,9 +126,9 @@ const Nav = () => {
         <div className="hover:cursor:pointer">
           <a href="#">Login</a>
         </div>
-        <div className="w-full">
+        <div className="w-full" onClick={(e) => e.preventDefault()}>
           <a
-            className="py-2 border-2 border-medium-gray inline-block w-full rounded-xl text-center hover:cursor:pointer"
+            className="py-2 border-2 border-medium-gray inline-block w-full rounded-xl text-center hover:cursor:pointer hover:bg-almost-black hover:text-almost-white hover:border-none"
             href="#"
           >
             Register
