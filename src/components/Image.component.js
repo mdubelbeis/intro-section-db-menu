1abbf30846c08873aed9ee69858ea11a5152995a
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 22:40:15 -0400

    mobile styling

---
+++ src/components/Image.component.js
@@ -0,0 +1,11 @@
+import imgSmall from '../images/image-hero-mobile.png';
+
+const Image = () => {
+  return (
+    <div className="mt-4">
+      <img src={imgSmall} alt="Artsy photo of guy on computer" />
+    </div>
+  );
+};
+
+export default Image;
