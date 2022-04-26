2fbed00b22e28e7e2fc3bd03ab342391e6547d65
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 22:41:20 -0400

    added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import Logo from '../images/logo.svg';
+import Menu from '../images/icon-menu.svg';
+
+import Image from './Image.component';
 const App = () => {
   return (
-    <div>
-      <h1>Challenge</h1>
+    <div className="py-6 px-4">
+      <header className="flex p-2 items-center justify-between">
+        <img className="pt-1" src={Logo} alt="" />
+        <img className="w-10" src={Menu} alt="" />
+      </header>
+
+      <main>
+        <Image />
+      </main>
     </div>
   );
 };
